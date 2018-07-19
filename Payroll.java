b7c5e070fd76d3198f88698d1b965ea2983d490b
NehaSwami <40225371+NehaSwami@users.noreply.github.com>
2018-07-19 12:57:16 +0530

    Add files via upload

--- Payroll.java
+++ Payroll.java
@@ -1,9 +1,58 @@
+import java.util.Scanner;
 
-public class Payroll {
-
-	public static void main(String[] args) {
+class Employee
+{
+	Scanner sc  = new Scanner(System.in);
+	String EmpName;
+	int EmpWage;
+	public void empName() 
+	{
+		System.out.println("Enter the employee name: ");
+		EmpName = sc.next();
+	}
+	public int empWage()
+	{
+		System.out.println("Enter the employee's hourly wage: ");
+		EmpWage = sc.nextInt();
+		return EmpWage;
+	}
+}
+class Employees
+{
+	int NoOfEmps;
+	public void employees() 
+	{
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("Enter the number of employees: ");
+		NoOfEmps = sc.nextInt();
+		Employee obj1 = new Employee();
+		obj1.empName();
+		obj1.empWage();
+	}
+}
+class TimeCard
+{
+	int NoOfTCs, NoOfMinsWorked;
+	public void timeCard() 
+	{
+		Scanner sc  = new Scanner(System.in);
+		System.out.println("Enter the number of time cards: ");
+		NoOfTCs = sc.nextInt();
 		
+		
+		
+	}
+}
+public class Payroll
+{
 
+	public static void main(String[] args) 
+	{
+		Employees obj2 = new Employees();
+		obj2.employees();
+		TimeCard obj3 = new TimeCard();
+		obj3.timeCard();
+		
 	}
 
 }
